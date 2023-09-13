12b877d22a597140275214269a38f652a25a55a4
Akash Kennedy <akashkennedy1@gmail.com>
2023-09-13 20:39:30 +0530

    Version 0.8

--- script.js
+++ script.js
@@ -119,5 +119,17 @@ currentProductSizes.forEach((size, index)=> {
         })
         size.style.backgroundColor = "black";
         size.style.color = "white";
-    })
-})
+    });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click",()=>{
+    payment.style.display = "none";
+});
